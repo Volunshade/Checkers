d530026a386f188010e453248cf600fa594b5de7
Thomas Newcomb <volunshade@gmail.com>
2023-01-11 18:24:43 -0800

    Updated checkerboard to allow less hard coding of squares.

--- checkers.py
+++ checkers.py
@@ -1,26 +1,38 @@
 """This module is for a game of checkers."""
-
 import tkinter as tk
 
-window = tk.Tk()
-window.geometry("800x800")
-window.title("Checkers")
 
-canvas = tk.Canvas(window, width=800, height=800)
+def main():
+    """Run the main process."""
 
-for row in range(8):
-    for column in range(8):
-        if (row + column) % 2 == 0:
-            color = "black"
-        else:
-            color = "red"
+    board_size = 800
+    grid_size = 8
 
-        row1 = row*100
-        column1 = column*100
-        row2 = row1+100
-        column2 = column1+100
-        canvas.create_rectangle(row1, column1, row2, column2, fill = color)
+    square_length = board_size / grid_size
 
-canvas.pack()
+    window = tk.Tk()
+    window.geometry(f"{board_size}x{board_size}")
+    window.title("Checkers")
 
-window.mainloop()
+    canvas = tk.Canvas(window, width=board_size, height=board_size)
+
+    for row in range(grid_size):
+        for col in range(grid_size):
+            if (row + col) % 2 == 0:
+                color = "black"
+            else:
+                color = "red"
+
+            row1 = row * square_length
+            col1 = col * square_length
+            row2 = row1 + square_length
+            col2 = col1 + square_length
+            canvas.create_rectangle(row1, col1, row2, col2, fill = color)
+
+    canvas.pack()
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
