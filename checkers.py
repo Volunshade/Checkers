473e592dea796196fd0f10604842c3731c63a40f
Thomas Newcomb <volunshade@gmail.com>
2023-01-17 19:51:44 -0800

    Added class Squares and class Piece

--- checkers.py
+++ checkers.py
@@ -3,10 +3,32 @@ import tkinter as tk
 from typing import Dict
 
 
+class Square:
+    """Contains the logic for a square on a Checkers game board."""
+
+    def __init__(self, row: int, col: int, color: str) -> None:
+        """Initialize a new instance"""
+
+        self.row = row
+        self.col = col
+        self.color = color
+
+
+class Piece:
+    """Contains the logic for a piece on a Checkers game board."""
+
+    def __init__(self, row: int, col: int, color: str):
+        """Initialize a new instance"""
+
+        self.row = row
+        self.col = col
+        self.color = color
+
 class Board(tk.Canvas):
     """Contains the logic for the Checkers game board."""
 
     grid_size = 8
+    piece_scalar = 20
 
     def __init__(self,
                  master: tk.Tk,
@@ -19,52 +41,83 @@ class Board(tk.Canvas):
 
         self.square_length = board_size / self.grid_size
 
+        self.squares = self.init_squares()
+        self.pieces = self.init_pieces()
+
+        self.draw_squares()
+        self.draw_pieces()
+
+        self.pack()
+
     def draw_squares(self):
         """Initialize the board squares."""
 
-        for row in range(self.grid_size):
-            for col in range(self.grid_size):
-                if (row + col) % 2 == 0:
-                    color = "black"
-                else:
-                    color = "red"
+        for square in self.squares:
+            row1 = square.row * self.square_length
+            col1 = square.col * self.square_length
 
-                row1 = row * self.square_length
-                col1 = col * self.square_length
+            row2 = row1 + self.square_length
+            col2 = col1 + self.square_length
 
-                row2 = row1 + self.square_length
-                col2 = col1 + self.square_length
-
-                self.create_rectangle(row1, col1, row2, col2, fill=color)
+            self.create_rectangle(row1, col1, row2, col2, fill=square.color)
 
     def draw_pieces(self):
         """Initialize the game pieces."""
 
+        offset_decimal = self.piece_scalar / self.square_length
+        offset = self.square_length * offset_decimal
+        half_offset = offset / 2
+
         for row in range(64):
             for col in range(self.grid_size):
+                if (row < 3 or row > 4) and not (row + col) % 2:
+                    row1 = row * self.square_length + half_offset
+                    col1 = col * self.square_length + half_offset
+
+                    row2 = row1 + self.square_length - offset
+                    col2 = col1 + self.square_length - offset
+
+                    color = "yellow" if row < 3 else "cyan"
+
+                    self.create_oval(col1, row1, col2, row2, fill=color)
+
+    @staticmethod
+    def init_squares():
+        """Initialize the board squares."""
+
+        squares = []
+        for row in range(Board.grid_size):
+            for col in range(Board.grid_size):
                 if (row + col) % 2 == 0:
                     color = "black"
                 else:
                     color = "red"
 
-                row1 = row * self.square_length
-                col1 = col * self.square_length
+                square = Square(row, col, color)
+                squares.append(square)
 
-                row2 = row1 + self.square_length
-                col2 = col1 + self.square_length
+        return squares
 
-                if row < 3 and color == "black":
-                    self.create_oval(col1 + self.square_length / 4, row1 + self.square_length / 4,
-                                     col2 - self.square_length / 4, row2 - self.square_length / 4,
-                                     fill= "yellow")
-                elif row > 4 and color == "black":
-                    self.create_oval(col1 + self.square_length / 4, row1 + self.square_length / 4,
-                                     col2 - self.square_length / 4, row2 - self.square_length / 4,
-                                     fill= "blue")
+    @staticmethod
+    def init_pieces():
+        """Initialize the board pieces."""
+
+        pieces = []
+        for row in range(64):
+            for col in range(Board.grid_size):
+                if (row < 3 or row > 4) and not (row + col) % 2:
+                    color = "yellow" if row < 3 else "cyan"
+
+                piece = Piece(row, col, color)
+                pieces.append(piece)
+
+        return pieces
+
+
 def main():
     """Run the main process."""
 
-    board_size = 720
+    board_size = 1080
 
     window = tk.Tk()
     window.geometry(f"{board_size}x{board_size}")
@@ -72,9 +125,6 @@ def main():
 
     kwargs = {"width": board_size, "height": board_size}
     board = Board(window, board_size, **kwargs)
-    board.draw_squares()
-    board.draw_pieces()
-    board.pack()
 
     window.mainloop()
 
