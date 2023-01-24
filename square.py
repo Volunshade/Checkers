de1dfd6772a63bc07a5294599b3ced0686f7ad65
Thomas Newcomb <volunshade@gmail.com>
2023-01-23 21:28:49 -0800

    Create Square module and copied Square class in from original Checkers module.

---
+++ square.py
@@ -0,0 +1,32 @@
+"""This module is for squares on a checkerboard."""
+import tkinter as tk
+
+
+class Square:
+    """Contains the logic for a square on a Checkers game board."""
+
+    side_length = -1.0
+
+    @classmethod
+    def update_side_length(cls, new_square_length: float) -> None:
+        """Update square length for all square instances."""
+
+        cls.side_length = new_square_length
+
+    def __init__(self, row: int, col: int, color: str) -> None:
+        """Initialize a new instance"""
+
+        self.row = row
+        self.col = col
+        self.color = color
+
+    def draw(self, canvas: tk.Canvas) -> None:
+        """Drawing a square on a canvas."""
+
+        row1 = self.row * self.side_length
+        col1 = self.col * self.side_length
+
+        row2 = row1 + self.side_length
+        col2 = col1 + self.side_length
+
+        canvas.create_rectangle(row1, col1, row2, col2, fill=self.color)
