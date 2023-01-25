1ff22c277906e6a4f46c3a9fc5ae820d7fb8562f
Adam Scanga <sandmanscanga@gmail.com>
2023-01-24 21:40:30 -0500

    Converted the __main__ file to be used only to launch the application.

--- checkers/__main__.py
+++ checkers/__main__.py
@@ -1,98 +1,10 @@
 """This module is for a game of checkers."""
 import tkinter as tk
-from typing import Dict
 
-from checkers.square import Square
-from checkers.piece import Piece
+from checkers.board import Board
 
 
-class Board(tk.Canvas):
-    """Contains the logic for the Checkers game board."""
-
-    grid_size = 8
-
-
-    def __init__(self,
-                 master: tk.Tk,
-                 board_size: int,
-                 **kwargs: Dict[str, int]
-                 ) -> None:
-        """Initialize a new instance."""
-
-        self.board_size = board_size
-
-        super().__init__(master, **kwargs)
-
-        self.bind("<Button-1>", self.left_click)
-
-        _square_length = board_size / self.grid_size
-        self.side_length = board_size / self.grid_size
-        Square.update_side_length(_square_length)
-        Piece.update_side_length(_square_length)
-
-        self.squares = self.init_squares()
-        self.pieces = self.init_pieces()
-
-        self.draw_squares()
-        self.draw_pieces()
-
-        self.pack()
-
-    def draw_squares(self):
-        """Initialize the board squares."""
-
-        for square in self.squares:
-            square.draw(self)
-
-    def draw_pieces(self):
-        """Initialize the game pieces."""
-
-        for piece in self.pieces:
-            piece.draw(self)
-
-    def left_click(self, event: tk.Event) ->  None:
-        """Callback function for the board left click event."""
-
-
-
-        col = int(event.x // self.side_length)
-        row = int(event.y // self.side_length)
-        print(event.x, col, " -> ", event.y, row)
-        pass
-
-    @staticmethod
-    def init_squares():
-        """Initialize the board squares."""
-
-        squares = []
-        for row in range(Board.grid_size):
-            for col in range(Board.grid_size):
-                if (row + col) % 2 == 0:
-                    color = "black"
-                else:
-                    color = "red"
-
-                square = Square(row, col, color)
-                squares.append(square)
-
-        return squares
-
-    @staticmethod
-    def init_pieces():
-        """Initialize the board pieces."""
-
-        pieces = []
-        for row in range(Board.grid_size):
-            for col in range(Board.grid_size):
-                if (row < 3 or row > (Board.grid_size - 4)) and not (row + col) % 2:
-                    color = "yellow" if row < 3 else "cyan"
-                    piece = Piece(row, col, color)
-                    pieces.append(piece)
-
-        return pieces
-
-
-def main():
+def main() -> None:
     """Run the main process."""
 
     board_size = 800
